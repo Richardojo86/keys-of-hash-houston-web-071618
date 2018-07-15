747d1629bf3f45ff4c95596b8896ee287943aac0
Richard Ojo <richardojo04@gmail.com>
2018-07-15 21:33:13 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+    arr = []
+    hash.collect{|key, value| if value == arguments 
+    arr << key 
+  end 
+    }
+  arr
 end
+end 
